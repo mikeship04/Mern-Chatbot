bdfe15637f4150f613c5436816841ada7b8e42b6
Mike Shippy <mikeship04@gmail.com>
2023-12-11 12:01:23 -0700

    connected to Mongo

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -1,2 +1,8 @@
-export {};
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log('Server Open And Connected to Database'));
+}).catch((error) => console.log(error));
 //# sourceMappingURL=index.js.map
---
+++ Backend/sr/index.js
@@ -0,0 +1,3 @@
+import app from "./app";
+app.listen(5000, () => console.log('Server Open'));
+//# sourceMappingURL=index.js.map
