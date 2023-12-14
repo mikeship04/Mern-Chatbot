0e0bff3f2fe262ed1bb4ace27e2733ad55c18776
Mike Shippy <mikeship04@gmail.com>
2023-12-14 11:07:32 -0700

    login/logout routing and user context

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
 import Toolbar from '@mui/material/Toolbar'
 import AppBar from '@mui/material/AppBar'
 import Logo from './shared/Logo'
+import { useAuth } from '../context/AuthContext'
+import NavigationLink from './shared/NavigationLink'
 
-function Header() {
+const Header = () => {
+  const auth = useAuth()
   return (
     <AppBar sx={{bgcolor: "transparent", position: "static", boxShadow: "none"}}>
       <Toolbar sx={{display: "flex"}}>
         <Logo/>
+        <div>
+          {auth?.isLoggedIn 
+          ?(<>
+              <NavigationLink bg = '#00fffc' to='/chat' text='Go to Chat' textColor='black' />
+              <NavigationLink bg = '#51538f' to='/' text='Logout' textColor='white' onClick={auth.logout} />
+            </>) 
+          : (<>
+              <NavigationLink bg = '#00fffc' to='/login' text='Login' textColor='black' />
+              <NavigationLink bg = '#51538f' to='/signup' text='Signup' textColor='white' onClick={auth.signup} />
+            </>)
+          }
+        </div>
       </Toolbar>
     </AppBar>
   )
--- Frontend/src/components/shared/Logo.tsx
+++ Frontend/src/components/shared/Logo.tsx
@@ -1,5 +1,5 @@
-import { Typography } from '@mui/material'
 import React from 'react'
+import { Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 function Logo() {
@@ -18,15 +18,15 @@ function Logo() {
           height={"30px"}
           className="image-inverted"
           />
-        <Typography sx={
-          {display:{md:"block", sm:"none", xs:"none"},
-            mr: "auto",
-            fontWeight: "800",
-            textShadow: "2px 2px 20px #000",
-          }}>
-            <span style={{ fontSize: "20px"}}>MERN</span>-GPT
-        </Typography>
       </Link>
+      <Typography sx={
+        {display:{md:"block", sm:"none", xs:"none"},
+          mr: "auto",
+          fontWeight: "800",
+          textShadow: "2px 2px 20px #000",
+        }}>
+          <span style={{ fontSize: "20px"}}>MERN</span>-GPT
+      </Typography>
     </div>
   )
 }
